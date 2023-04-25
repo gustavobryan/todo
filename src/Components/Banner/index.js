d5a0fb3af3bc3547bd27e8789c3218567bc79a62
Gustavo Bryan <devgustavob@gmail.com>
2023-04-25 15:45:06 -0300

    banner

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,10 @@
+import './Banner.css';
+
+function Banner () {
+    return (
+        <div className='banner'>
+            <img src='/imagens/banner.png' alt='logo to do'/>
+        </div>
+    )
+}
+export default Banner;
